ef29ebeea31023180c2d7b1cbf23c5d0439e3eed
C87m <chika87ma@gmail.com>
2026-04-27 16:19:19 +0900

    add Ex_02_03

---
+++ Ex_02_03/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+namespace Ex_02_03;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("数値を入力してください->");
+        string s = Console.ReadLine() ?? "";
+        double num = double.Parse(s);
+
+        Console.WriteLine($"5を加算した結果は{num+5}です");
+        Console.WriteLine($"5を減算した結果は{num-5}です");
+    }
+}
