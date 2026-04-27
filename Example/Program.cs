b69bade1064f38306e664ee9ab72ced67e6d0619
C87m <chika87ma@gmail.com>
2026-04-27 15:14:08 +0900

    add Ex_02_02

--- Example/Program.cs
+++ Example/Program.cs
@@ -4,6 +4,14 @@ static class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello");
+
+        //Console.WriteLine(Console.Read());
+
+        //Console.WriteLine(Console.ReadKey());
+
+        Console.WriteLine(Console.ReadLine());
+
+
     }
 }
