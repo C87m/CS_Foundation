69f166a22086874b65ad126d8fb09227183a6659
C87m <chika87ma@gmail.com>
2026-04-27 16:58:33 +0900

    add coment

--- Ex_02_03/Program.cs
+++ Ex_02_03/Program.cs
@@ -7,6 +7,7 @@ static class Program
     static void Main(string[] args)
     {
         Console.Write("数値を入力してください->");
+        // 数値じゃなかった場合の処理を入れたいところ
         string s = Console.ReadLine() ?? "";
         double num = double.Parse(s);
 
