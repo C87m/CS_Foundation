576068927f5ccef5d61e239fa1ef41c00f00f8d8
C87m <chika87ma@gmail.com>
2026-04-28 10:51:58 +0900

    add Ex2-7

---
+++ Ex_02_07/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Ex_02_07;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("数値を入力してください->");
+        double num = double.Parse(Console.ReadLine()  ?? "");
+
+        if(num > 0)
+        {
+            Console.WriteLine("正の値です。");
+        }
+        else if(num < 0)
+        {
+            Console.WriteLine("負の値です。");
+        }
+        else
+        {
+            Console.WriteLine("0です。");
+        }
+    }
+}
