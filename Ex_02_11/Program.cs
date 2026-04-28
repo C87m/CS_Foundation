7e5949160fdd97c52e08f2c56c1c33cccbedb4cb
C87m <chika87ma@gmail.com>
2026-04-28 11:23:13 +0900

    add Ex2-11

---
+++ Ex_02_11/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Ex_02_11;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        string[] stations = ["品川","大崎","五反田","目黒","恵比寿","渋谷","原宿","代々木","新宿"];
+
+        Console.Write("駅名を入力してください->");
+        string rm_station = Console.ReadLine();
+
+        foreach(string station in stations)
+        {
+            if(station == rm_station)
+            {
+                continue;
+            }
+            Console.WriteLine(station);
+        }
+    }
+}
