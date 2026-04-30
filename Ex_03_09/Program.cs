906a2a0cb69be9ebbd9566a87b39f1aefe4e1403
C87m <chika87ma@gmail.com>
2026-04-30 15:36:11 +0900

    add Ex3-9

---
+++ Ex_03_09/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Drawing;
+using System.Runtime.InteropServices.Marshalling;
+using Ex_03_09.Modules;
+namespace Ex_03_09;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("口座番号を入力してください->");
+        string accountNo = Console.ReadLine();
+        Console.Write("口座名義を入力してください->");
+        string accountName = Console.ReadLine();
+        Console.Write("残高を入力してください->");
+        int balance = int.Parse(Console.ReadLine() ?? "0");
+        Console.Write("ポイントを入力してください->");
+        int point = int.Parse(Console.ReadLine() ?? "0");
+
+        NetAccount user = new(accountNo, accountName, balance, point);
+
+        user.Print();
+
+        Console.WriteLine(user.ToString());
+
+    }
+}
--- Example/Program.cs
+++ Example/Program.cs
@@ -4,10 +4,10 @@ static class Program
 {
     static void Main(string[] args)
     {
-        Warrior character = new();
-        character.Name = "戦士";
-        character.Hp = 10;//setアクセッサがprivateなので設定不可
-        character.Ap = 10;//読み取り専用プロパティなので設定不可
-
+        Human human = new();
+        string s = human.ToString();
+        Console.WriteLine(s);
+        object o = human;
+        Console.WriteLine(o.Equals(human));
     }
 }
