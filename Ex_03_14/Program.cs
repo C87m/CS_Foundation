0bf9e7ad5cbba74689b761b1e9ccc80ca03be0e9
C87m <chika87ma@gmail.com>
2026-05-01 11:24:29 +0900

    add Ex3-14

---
+++ Ex_03_14/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Ex_03_14;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("値1を入力してください->");
+        int num1 = int.Parse(Console.ReadLine()  ?? "0");
+        Console.Write("値2を入力してください->");
+        int num2 = int.Parse(Console.ReadLine()  ?? "0");
+
+        Console.WriteLine($"加算： {num1.Add(num2)}");
+        Console.WriteLine($"平均： {num1.Average(num2)}");
+        
+    }
+}
