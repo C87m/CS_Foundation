63a5d118bcbb853776b37d466407f2a9c674b8a7
C87m <chika87ma@gmail.com>
2026-05-01 15:25:00 +0900

    add Ex4-2

---
+++ Ex_04_02/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Ex_04_02;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("年を入力してください->");
+        int year = int.Parse(Console.ReadLine() ?? "2026");
+
+        if (DateTime.IsLeapYear(year))
+        {
+            Console.WriteLine($"{year}年は閏年です。");
+        }
+        else
+        {
+            Console.WriteLine($"{year}年は閏年ではありません。");
+        }
+
+    }
+}
